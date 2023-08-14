25f129fefb191cb296b9e645072496f206430c2f
Dan Berger <daniel.berger.hull@gmail.com>
2023-08-14 19:45:24 -0400

    cleaning the second folder not used

--- bellman-ford2/web/view/RenderingConstants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.RANDOM_GRAPH_RENDERING = exports.CONCENTRIC_GRAPH_RENDERING = exports.CIRCULAR_GRAPH_RENDERING = void 0;
-exports.CIRCULAR_GRAPH_RENDERING = 1;
-exports.CONCENTRIC_GRAPH_RENDERING = 2;
-exports.RANDOM_GRAPH_RENDERING = 3;
